e0360b852a079df46d0acb400ed8b388cf3d8ef6
Jason Dew <jason.dew@gmail.com>
2015-04-26 19:10:53 -0400

    Fixing up completed chores

--- app/controllers/completed_chores_controller.rb
+++ app/controllers/completed_chores_controller.rb
@@ -1,11 +1,27 @@
 class CompletedChoresController < ApplicationController
   def create
-    CompletedChore.create! chore_id: params[:id], completed_on: params[:day]
-    redirect_to root_path, notice: praise.sample
+    completed_chore = CompletedChore.new attributes
+
+    if completed_chore.save
+      render json: completed_chore
+    else
+      render json: {errors: completed_chore.errors}
+    end
   end
 
   def destroy
-    CompletedChore.find_by(chore_id: params[:id], completed_on: params[:day]).destroy!
-    redirect_to root_path, notice: "Chore has been undone."
+    completed_chore = CompletedChore.find_by(chore_id: params[:id], completed_on: params[:day])
+
+    if completed_chore.destroy
+      render json: completed_chore
+    else
+      render json: {errors: completed_chore.errors}
+    end
+  end
+
+  private
+
+  def attributes
+    params.require(:completed_chore).permit(:chore_id, :completed_on)
   end
 end
--- app/models/completed_chore.rb
+++ app/models/completed_chore.rb
@@ -1,4 +1,7 @@
 class CompletedChore < ActiveRecord::Base
   belongs_to :child
   belongs_to :chore
+
+  validates_presence_of :chore_id, :completed_on
+  validates_uniqueness_of :completed_on, scope: [:child_id, :chore_id]
 end
